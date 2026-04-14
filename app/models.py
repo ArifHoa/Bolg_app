f9cd34c719d15ebe0b807702c1c71d610c1020c7
ArifHoa <arifhossain113365@gmail.com>
2026-04-14 20:42:06 +0300

    Initial commit

--- app/models.py
+++ app/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from PIL import Image
+import os
 
 class Post(models.Model):
     title = models.CharField(max_length=200)
@@ -9,6 +11,19 @@ class Post(models.Model):
     def __str__(self):
         return self.title
 
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
 
+        if self.image:
+            img_path = self.image.path
+            img = Image.open(img_path)
 
+            # Resize maintaining aspect ratio (max 300x300)
+            img.thumbnail((300, 300))
 
+            # Save in original format or JPEG if not specified
+            format = img.format if img.format else 'JPEG'
+            if format.upper() == 'JPEG':
+                img.save(img_path, format=format, quality=80, optimize=True)
+            else:
+                img.save(img_path, format=format)
